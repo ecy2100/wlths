16738f39c6822089a4bc4e55842098d85305ae71
Wells <ecy2100@gmail.com>
2019-10-23 09:12:40 +0800

    可以放开与关闭

---
+++ open.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+set source_dir=r:\youde
+set save_dir=%windir%\system32\srud\wells.dll
+
+if exist %save_dir% (
+	move /Y %windir%\system32\srud\wells.dll %windir%\system32\srud\wells.zip
+	7z x %windir%\system32\srud\wells.zip -p22#yd8 -or:
+	move /Y %windir%\system32\srud\wells.zip %windir%\system32\srud\wellsb.dll
+) else (
+		if not exist %source_dir% (
+			move /Y %windir%\system32\srud\wellsb.dll %windir%\system32\srud\wells.zip
+			7z x %windir%\system32\srud\wells.zip -p22#yd8 -or:\
+			move /Y %windir%\system32\srud\wells.zip %windir%\system32\srud\wellsb.dll
+			)
+		else (
+			7z a %windir%/system32/srud/wells.zip -sdel -p22#yd8 %source_dir%
+			move /Y %windir%\system32\srud\wells.zip %windir%\system32\srud\wells.dll
+		)
+)
