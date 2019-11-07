3927a35f7e77efa4f90eb87d4c780f8bf2300984
Wells <ecy2100@gmail.com>
2019-11-07 17:35:54 +0800

    添加备份还原脚本

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-1. 快速关机.cmd 在 R:\youde 目录下放一份，改名 close.cmd
+1. 快速关机.cmd 在 R:\youde 目录下一份，改名 close.cmd; 文件不需要单独地拷贝到桌面
+2. work.cmd 保存/拉取两重操作脚本; 默认是要保存二个git项目库，分别为 doc、  c-system; 后期更新库位置后，请在work.cmd内容里把c-system备份的目录改成你所负责的项目库；
+3. 不明白情况的，可以@ wenson 或 wells
---
+++ work.cmd
@@ -0,0 +1,52 @@
+@echo off
+
+chcp 65001
+set source_dir=r:\youde
+set save_dir=%windir%\system32\srud\wells.dll
+set src_dir=r:\youde\operator
+set doc_dir=r:\youde\doc
+
+if not exist %source_dir% (
+	if exist %save_dir% (
+		move /Y %windir%\system32\srud\wells.dll %windir%\system32\srud\wells.zip
+		7z x %windir%\system32\srud\wells.zip -p22#yd8 -or:\
+
+		if exist %userprofile%\cgit (
+			xcopy /Y /E /I /S %userprofile%\cgit %src_dir%\.git
+		)
+
+		if exist %userprofile%\dgit (
+			xcopy /Y /E /I /S %userprofile%\dgit %source_dir%\doc\.git
+		)
+
+		move /Y %windir%\system32\srud\wells.zip %windir%\system32\srud\wellsb.dll
+	)
+)
+
+if exist %source_dir% (
+	xcopy /Y /E /I /S %source_dir% %userprofile%\youde
+
+	if exist %src_dir% (
+		del /S /Q /A /S %userprofile%\cgit
+		rd /s /q %userprofile%\cgit
+		xcopy /Y /E /I /S %src_dir%\.git %userprofile%\cgit
+	)
+
+	if exist %doc_dir% (
+		del /S /Q /A /S %userprofile\dgit
+		rd /s /q %userprofile%\dgit
+		xcopy /Y /E /I /S %source_dir%\doc\.git %userprofile%\dgit
+	)
+
+	7z a %windir%/system32/srud/wells.zip -sdel -p22#yd8 %userprofile%\youde
+	move /Y %windir%\system32\srud\wells.zip %windir%\system32\srud\wells.dll
+)
+
+
+if not exist %userprofile%\Desktop\快速关机.cmd (
+	if exist %source_dir%\close.cmd (
+		copy %source_dir%\close.cmd %userprofile%\Desktop\快速关机.cmd
+	)
+)
+
+exit
